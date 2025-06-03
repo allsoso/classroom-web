ab8de9865c01f6a9eec5b92908931b54f841ce21
Allan Vasconcelos <avfsouza@gmail.com>
2025-06-03 07:16:03 -0300

    fix:missing model import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from '../users/models/user.model';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    SequelizeModule.forFeature([User]),
+    JwtModule.registerAsync({
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: '15m' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
