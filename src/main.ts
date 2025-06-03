5d190b6fcbb6e48dc69bf25af309acdd4f2234ac
Allan Vasconcelos <avfsouza@gmail.com>
2025-06-03 07:17:03 -0300

    feat:swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
+  const config = new DocumentBuilder()
+    .setTitle('Classroom API')
+    .setDescription('API for the Classroom application')
+    .setVersion('1.0')
+    .addTag('classroom')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
